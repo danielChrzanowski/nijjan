dbeb39227bc06627b9c53638da8e42f41a90993c
Daniel <daniel_chrzanowski98@wp.pl>
2022-04-10 18:02:53 +0200

    dark mode, carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import './App.scss';
-import Home from './components/Home/Home';
-import Contact from './components/Contact/Contact';
+import { faPalette } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import useLocalStorage from 'use-local-storage';
+import './App.scss';
+import Contact from './components/Contact/Contact';
 import GW2 from './components/Games/GW2/GW2';
+import Home from './components/Home/Home';
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
       <BrowserRouter>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
@@ -17,23 +28,26 @@ function App() {
               <Nav className="me-auto">
                 <Nav.Link as={Link} to="/contact">Features</Nav.Link>
                 <NavDropdown title="Games" id="collasible-nav-dropdown">
-                  <NavDropdown.Item as={Link} to="/games/gw2">Guild Wars 2</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/contact">All games</NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item as={Link} to="/contact">Separated link</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/games/gw2">Guild Wars 2</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
               <Nav>
                 <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                <Nav.Link onClick={switchTheme}><FontAwesomeIcon icon={faPalette} /></Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/games/gw2' element={<GW2 />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
+        
+        <div className="content">
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/games/gw2' element={<GW2 />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </div >
       </BrowserRouter>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,25 @@
+import { Carousel } from 'react-bootstrap';
+import image1 from '../../assets/gw2_1.jpg';
+import image2 from '../../assets/gw2_2.jpg';
+import image3 from '../../assets/gw2_3.jpg';
 import './Home.scss';
 
 const Home = () => (
   <div className='Home'>
+    <div className='carousel'>
+      <Carousel fade>
+        <Carousel.Item>
+          <img className="d-block w-100" src={image1} alt=""/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={image2} alt=""/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={image3} alt=""/>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+
     Home Component
   </div>
 );
